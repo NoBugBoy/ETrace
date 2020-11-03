74f6704ff7dd00a0bce4427aa9bdb00ece9ea6c7
YuJian <754369677@qq.com>
2020-11-03 17:58:10 +0800

    1.1版本添加aop功能

--- src/main/java/com/agent/wsagent/CheckAgent.java
+++ src/main/java/com/agent/wsagent/CheckAgent.java
@@ -2,7 +2,6 @@ package com.agent.wsagent;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.StreamProgress;
-import cn.hutool.core.lang.Console;
 import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
